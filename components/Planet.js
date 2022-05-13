f3627bd0e5006cd32374b4c339b873bfba886af1
Silsor <trollciu@gmail.com>
2022-05-13 16:39:37 +0200

    planety wrzucone

--- components/Planet.js
+++ components/Planet.js
@@ -1,27 +1,28 @@
 //----------------------------------------------------------------------------------
 //--- Sun
 //----------------------------------------------------------------------------------
-let clock;
+
 AFRAME.registerComponent('planet', {
 
     schema:
     {
         model: {},
-        a: { type: 'number', default: 20 },
+        a: { type: 'number', default: 3},
         b: { type: 'number', default: 5 },
         t: { type: 'number', default: 0 },
         time: { type: 'number', default: 10},
-        rotationSpeed: { type: 'number', default: 1 }
+        rotationSpeed: { type: 'number', default: 4 },
+        clock: {}
     },
 
     init: function () {
         this.data.model = this.el.object3D;
-        clock = new THREE.Clock()
+        this.data.clock = new THREE.Clock();
     },
 
     tick: function () {
-        let delta = clock.getDelta();
-        this.data.t += delta / (this.data.time *1.0);
+        let delta = this.data.clock.getDelta();
+        this.data.t += delta / (this.data.time * 1.0);
 
         if (Math.abs(this.data.t) > 1) this.data.time *= -1
 
