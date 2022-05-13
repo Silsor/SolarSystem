9db6aaea8b46bb9c2a61790f39687f5182f7f6d6
Patszcz <32665871+Patszcz@users.noreply.github.com>
2022-05-13 14:54:37 +0200

    update

--- components/Planet.js
+++ components/Planet.js
@@ -7,28 +7,25 @@ AFRAME.registerComponent('planet', {
     schema:
     {
         model: {},
-        a: { type: 'number', default: 6 },
-        b: { type: 'number', default: 3 },
+        a: { type: 'number', default: 20 },
+        b: { type: 'number', default: 5 },
         t: { type: 'number', default: 0 },
-        time: { type: 'number', default: 10 },
+        time: { type: 'number', default: 10},
         rotationSpeed: { type: 'number', default: 1 }
     },
 
     init: function () {
         this.data.model = this.el.object3D;
         clock = new THREE.Clock()
-        console.log(this.data.model);
     },
 
     tick: function () {
         let delta = clock.getDelta();
-        this.data.t += delta / this.data.time;
+        this.data.t += delta / (this.data.time *1.0);
 
-        if (Math.abs(this.data.t) > 1) this.data.time *= -this.data.time;
-        this.data.model.position.set(this.data.a * (1 - Math.abs(this.data.t)) * this.data.time > 0 ? 1 : -1, 0, this.data.b * this.data.t);
+        if (Math.abs(this.data.t) > 1) this.data.time *= -1
 
+        this.data.model.position.set(this.data.a * (1 - Math.abs(this.data.t) * this.data.time > 0 ? 1 : -1), 0, this.data.b * this.data.t);
         this.data.model.rotation.y += this.data.rotationSpeed * delta;
-        console.log(this.data.x + " " + this.data.y);
-
     }
 });
