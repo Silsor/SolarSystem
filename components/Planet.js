a8db400d2885eef30ad745887f21f383cfbb499b
Silsor <trollciu@gmail.com>
2022-05-16 17:03:10 +0200

    Update Planet.js
    teraz nie ma przeskoku

--- components/Planet.js
+++ components/Planet.js
@@ -7,33 +7,32 @@ AFRAME.registerComponent('planet', {
     schema:
     {
         model: {},
-        a: { type: 'number', default: 2},
-        b: { type: 'number', default: 10 },
-        t: { type: 'number', default: 0 },
-        time: { type: 'number', default: 15},
-        rotationSpeed: { type: 'number', default: 4 },
-        clock: {}
+        a: { type: 'number', default: 10}, // max odleglosc elipsy w osi x
+        b: { type: 'number', default: 5 }, // max odleglosc elipsy w osi y
+        x: { type: 'number', default: 10 },
+        timeToAroundSun: {type: 'number', default: 1},
+        rotationSpeed: { type: 'number', default: 2 },
+        clock: {},
+        duration: {type: 'number', default: 1},
+        t: {}
     },
 
     init: function () {
         this.data.model = this.el.object3D;
         this.data.clock = new THREE.Clock();
+        this.data.t = 0;
     },
 
     tick: function () {
         let delta = this.data.clock.getDelta();
-        this.data.t += delta / (this.data.time * 1.0);
+        this.data.t += delta;
+        let lastX = this.data.x;
+        this.data.x = Math.sin(this.data.t * Math.PI / 10) * this.data.a;
+        
+        let y = Math.sqrt((1 - Math.pow(this.data.x, 2) / Math.pow(this.data.a, 2)) * Math.pow(this.data.b, 2));
+        if (lastX > this.data.x) y *= -1;
 
-        if (this.data.t > 1 && this.data.time > 0) {
-			this.data.time *= -1;		
-			this.data.t = 1;
-		}
-        else if (this.data.t < -1 && this.data.time < 0) {
-			this.data.time *= -1;		
-			this.data.t = -1;
-		}
-
-        this.data.model.position.set(this.data.a * (1 - Math.abs(this.data.t)) * ((this.data.time > 0) ? 1 : -1), 0, this.data.b * this.data.t);
-        this.data.model.rotation.y += this.data.rotationSpeed * delta;
+        this.data.model.position.set(this.data.x, 0, y);
+        this.data.model.rotation.y += this.data.rotationSpeed;
     }
 });
