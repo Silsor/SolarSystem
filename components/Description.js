422f299b5b42deeb660902d358c1e9093a2f4c5a
Silsor <trollciu@gmail.com>
2022-05-24 16:44:06 +0200

    Update Description.js

--- components/Description.js
+++ components/Description.js
@@ -9,17 +9,34 @@
 - Display formated name
 - Remove after 2048 ms
 */
-
+let showing = false;
 AFRAME.registerComponent('description', {
     // Allow line component to accept vertices and color.
     schema: {
         color: { default: '#333' },
         name: {default: ''},
         parent: {default: 'camera'},
-        info: {}
+        info: {}, 
+        objectInfo: {}
     },
     init: function(){
-    console.log("init from " + this.data.name);
+    console.log("init from " + this.el.id);
+
+    this.el.addEventListener('click', () => 
+        {   
+            showInfo(this);
+        });
+
+     function showInfo(object)
+     {
+        if (!showing)
+            {
+                showing = true;
+                object.data.objectInfo.object3D.visible = true; 
+                object.data.objectInfo.classList.add("clickable");
+                console.log(showing);
+            }
+     }
     },
   
     // Create or update the line geometry.
@@ -33,9 +50,9 @@ AFRAME.registerComponent('description', {
             // show part name
             let text = `${partName.toUpperCase()}` + "\n" + this.data.info;
             let match = text.match(/.{1,18}/g); //how many lines
-            let offsetTXT = match.length==1 ? 0: -0.1*0.25*match.length;
-          
-            const geometry = new THREE.PlaneGeometry(1, match.length/3);
+            //let offsetTXT = 1;
+          console.log(match.length);
+            const geometry = new THREE.PlaneGeometry(1.5, 2);
             const loader = new THREE.TextureLoader();
 
             const material = new THREE.MeshBasicMaterial({
@@ -47,30 +64,30 @@ AFRAME.registerComponent('description', {
 
             this.entityEl = document.createElement('a-entity');
             let entityEl = this.entityEl;
-            entityEl.setObject3D("part-name", plane);
-            
-            entityEl.setAttribute('id', 'part-name');
+            entityEl.setObject3D("desc-"+this.data.name, plane);
+            entityEl.object3D.visible = showing ? false : true;
+            entityEl.setAttribute('id', 'desc:' + this.data.name);
             entityEl.setAttribute('scale', '0.025, 0.025, 0.025');
             entityEl.setAttribute('material' , {'opacity': 0});
             entityEl.setAttribute('text', {
                 value: text,
                 color: "#000000",
                 align: "center", 
-                wrapCount: 18, 
+                wrapCount: 26, 
                 negate: false,
                 font: "https://cdn.glitch.com/d13178b1-0ea9-4338-acf4-7705e127ccde%2FExo-SemiBold-msdf.json?v=1620296905072",
                 shader: "msdf"
             });
             entityEl.setAttribute('overlay');
+            if (!showing) entityEl.classList.add("clickable");
 
             //calculate offset of texture to make decor line the same height            
            // const { material1, geometry1 } = entityEl.getObject3D('mesh');
-            material.map.offset.y = (match.length==1) ? 0: -0.1*0.25*match.length;
+            //material.map.offset.y = (match.length==1) ? 0: -0.05*0.1*match.length;
             material.needsUpdate = true;
 
             document.querySelector('[camera]').appendChild(entityEl);
             entityEl.object3D.position.set(0, 0.015, -0.05);
-            console.log(entityEl);           
             entityEl.setAttribute('animation', {
                 property: 'material.opacity',
                 from: 0,
@@ -78,6 +95,16 @@ AFRAME.registerComponent('description', {
                 dur: 250,
                 easing: 'easeOutCubic'
             });
+            this.data.objectInfo = entityEl;
+            console.log(this.data.objectInfo);
+            this.data.objectInfo.addEventListener('click', () => { hideInfo(this.data.objectInfo)});
+        }
+
+        function hideInfo(infoObj)
+        {
+            showing = false; 
+            infoObj.object3D.visible = false;
+            infoObj.classList.remove("clickable");
         }
     },
   
